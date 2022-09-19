fb508581d5077fba57eb08b818f793d7f86444cd
Sviatoslav <ecrprague.asi@gmail.com>
2022-09-19 17:20:30 +0300

    Task01 Done

---
+++ Task01/Program.cs
@@ -0,0 +1,6 @@
+﻿int num=new Random().Next(10,100);
+Console.WriteLine($"Случайное число равно: {num}");
+int firstDigit=num/10;
+int secondDigit=num%10;
+if (firstDigit>secondDigit) Console.WriteLine($"=>{firstDigit}");
+else Console.WriteLine($"=>{secondDigit}");
