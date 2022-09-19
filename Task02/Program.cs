8986d0a461cd247bf984059164f447d21bdf764a
Sviatoslav <ecrprague.asi@gmail.com>
2022-09-19 19:12:05 +0300

    Task02 D

--- Task02/Program.cs
+++ Task02/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int num=new Random().Next(100,1000);
+
+Console.WriteLine($"Введенное число: {num}");
+
+
+
+// int num1=num/100;
+// int num3=num%10;
+// Console.WriteLine($"Вывод: {num1}{num3}");
+
+int mtd(int numero)
+{
+    int n1=numero/100;
+    int n3=numero%10;
+    return n1*10+n3;
+}
+int result=mtd(num);
+Console.WriteLine($"Вывод: {result}");
